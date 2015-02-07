c7870d6941c84a6e2172db6c531fd177cede259c
crystalDf <chendong333@gmail.com>
2015-02-07 19:04:41 +0800

    Chapter 32\DragAndDraw finished

---
+++ app/src/main/java/com/star/draganddraw/DragAndDrawActivity.java
@@ -0,0 +1,17 @@
+package com.star.draganddraw;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class DragAndDrawActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new DragAndDrawFragment();
+    }
+
+}
